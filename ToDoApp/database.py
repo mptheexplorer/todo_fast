546a95c0d31b37c36880f837a1a095269b2342b9
Mahendra Pratap Singh <126226680+mptheexplorer@users.noreply.github.com>
2024-06-25 11:26:18 +0000

    database connection

---
+++ ToDoApp/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine # type: ignore
+from sqlalchemy.orm import sessionmaker  # type: ignore
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base() # type: ignore
